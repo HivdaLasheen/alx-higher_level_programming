e2eddd6079967bd41404058d7ce3a821f486a167
HivdaLasheen <hivdahollz@gmail.com>
2024-05-23 17:51:27 -0700

    Add script to display top 3 cities by temperature during July and August

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,2 @@
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperature_data
-WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+-- LIST TOP THREE CITIES
+SELECT city, AVG(temperature) AS avg_temp FROM temperature_data WHERE MONTH(date) IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
