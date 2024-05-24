839f09f5667dbadae8d682b53d4fc79380a182a1
HivdaLasheen <hivdahollz@gmail.com>
2024-05-23 19:09:29 -0700

    cities

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- Select cities with their corresponding state names
+SELECT cities.id, cities.name, (
+    SELECT states.name
+    FROM states
+    WHERE states.id = cities.state_id
+) AS state_name
+FROM cities
+ORDER BY cities.id ASC;
