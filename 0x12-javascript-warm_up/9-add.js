b8ec7e56984ea7e1cbf2529f2dea55e0979c0606
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 05:49:06 +0000

    add commit

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-function add(a, b) {
+
+function add (a, b) {
   console.log(a + b);
 }
+
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
+
 add(arg1, arg2);
