28b036e85860f7a9246ec9d774a779ffb677996a
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 09:45:03 +0000

    1-rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
+// module.exports = class Rectangle {
+//   constructor (w, h) {
+// this.width = w;
+// this.height = h;
+//   }
+// };
+
+// OR
+class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
   }
-};
+}
+
+module.exports = Rectangle;
