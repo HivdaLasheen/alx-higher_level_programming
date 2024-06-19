23b016c388ef6ff14c62843468365adcc1aaae7a
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 09:56:31 +0000

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,28 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor(w, h) {
+class Rectangle {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
-      // Create an empty object if w or h is not a positive integer or equal to 0
+      // Create an empty object if width or height are not valid
       this.width = undefined;
       this.height = undefined;
     }
   }
 
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
+  print () {
+    if (!this.width || !this.height) {
+      return;
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
       }
+      console.log(row);
     }
   }
-};
+}
+
+module.exports = Rectangle;
