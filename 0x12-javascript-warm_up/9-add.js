d2509f99ff8dfeb5875398e17b3dc78f9153f5cf
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 01:59:53 +0000

    adjust 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
+
 function addIntegers(num1, num2) {
   return num1 + num2;
 }
-console.log(addIntegers(Number(process.argv[2]), Number(process.argv[3])));
+
+const firstInt = parseInt(process.argv[2]);
+const secondInt = parseInt(process.argv[3]);
+
+if (!isNaN(firstInt) && !isNaN(secondInt)) {
+  console.log(addIntegers(firstInt, secondInt));
+} else {
+  console.log('Please provide two valid integers.');
+}
