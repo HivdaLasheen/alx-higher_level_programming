362b9738b8f0fc5c1b2a88ffe844688087c0c6f5
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 10:00:19 +0000

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,43 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor(w, h) {
+class Rectangle {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
-      // Create an empty object if w or h is not a positive integer or equal to 0
+      // Create an empty object if width or height are not valid
       this.width = undefined;
       this.height = undefined;
     }
   }
 
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
+  print () {
+    if (!this.width || !this.height) {
+      return;
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
       }
+      console.log(row);
     }
   }
 
-  rotate() {
-    if (this.width && this.height) {
-      [this.width, this.height] = [this.height, this.width];
+  double () {
+    if (!this.width || !this.height) {
+      return;
     }
+    this.width *= 2;
+    this.height *= 2;
   }
 
-  double() {
-    if (this.width && this.height) {
-      [this.width, this.height] = [this.width * 2, this.height * 2];
+  rotate () {
+    if (!this.width || !this.height) {
+      return;
     }
+    [this.width, this.height] = [this.height, this.width];
   }
-};
+}
+
+module.exports = Rectangle;
