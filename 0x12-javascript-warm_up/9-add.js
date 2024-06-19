0dba565abb40d48366141530fb853d32b355b939
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 02:43:16 +0000

    commit

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 function add(a, b) {
   if (!isNaN(a) && !isNaN(b)) {
     console.log(a + b);
@@ -7,8 +6,6 @@ function add(a, b) {
     console.log('Invalid input. Please provide two integers.');
   }
 }
-
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
-
 add(arg1, arg2);
