996e3586ac14e32034965b2708885a7f6318e3f1
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 01:56:21 +0000

    adjust 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-
-function add(a, b) {
-  return a + b;
+function addIntegers(num1, num2) {
+  return num1 + num2;
 }
-
-const firstInteger = Number(process.argv[2]);
-const secondInteger = Number(process.argv[3]);
-
-if (isNaN(firstInteger) || isNaN(secondInteger)) {
-  console.log('Arguments are not valid integers');
-} else {
-  console.log(`The addition of ${firstInteger} and ${secondInteger} is: ${add(firstInteger, secondInteger)}`);
-}
+console.log(addIntegers(Number(process.argv[2]), Number(process.argv[3])));
