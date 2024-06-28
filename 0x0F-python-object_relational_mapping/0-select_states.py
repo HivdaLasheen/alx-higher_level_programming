d4695ed4d31034ce0f59b6cfbce0578dfba628d9
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-28 17:44:24 +0000

    select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""
+This script lists all states from the database hbtn_0e_0_usa.
+It takes three arguments: MySQL username, MySQL password,
+and the database name. The results are sorted in ascending order
+by states.id.
+"""
+
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    import MySQLdb
-    import sys
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
 
-    # Establishing a connection to the MySQL database
-    database = MySQLdb.connect(host='localhost', port=3306,
-                               user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # Execute the SQL query to retrieve all states sorted by id
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
 
-    # Creating a cursor object to interact with the database
-    cursor = database.cursor()
+    # Fetch all the rows from the result of the query
+    rows = cur.fetchall()
 
-    # Executing the SQL query to retrieve all states sorted by id
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    # Iterate through the rows and print each one
+    for row in rows:
+        print(row)
 
-    # Fetching all the rows from the result of the query
-    results = cursor.fetchall()
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
 
-    # Iterating through the rows and printing each one
-    for state in results:
-        print(state)
-
-    # Closing the cursor and database connection
-    cursor.close()
-    database.close()
