e049bc1df8bc617db23c9e8ffa2ed912c5be42c9
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-28 21:49:37 +0000

    filterXMapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the SQL query to retrieve states with names starting with 'N'
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    # Fetch all the rows from the result of the query
+    rows = cur.fetchall()
+
+    # Iterate through the rows and print each one
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
