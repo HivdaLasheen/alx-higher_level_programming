de211cbb87848c896eadeb4550d3a3c6f65d5dbd
Hivda <125532618+HivdaLasheen@users.noreply.github.com>
2024-08-17 16:14:43 +0000

    add

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+# 8-uppercase.py
+
+
 
 def uppercase(str):
-    """print a string in uppercase."""
+    """Print a string in uppercase."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-            print("{}".format(c), end="")
-            print("")
+        print("{}".format(c), end="")
+    print("")
